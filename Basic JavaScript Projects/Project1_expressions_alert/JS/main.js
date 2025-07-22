30c8dbc11a0aa7495e1d0a05a11939142cfbe927
aminullah-dev <Aminhashemi979@gmail.com>
2025-07-22 18:46:31 -0400

    Add Basic JavaScript Projects folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+// Alert example
+window.alert("Hello, this is an alert!");
+
+// document.write example
+document.write("Hello from document.write!");
+
+// Variable assignment
+var greeting = "Hello, world!";
+document.write(greeting);
+
+// Concatenation
+var part1 = "Hello";
+var part2 = "World";
+document.write(part1 + " " + part2);
+
+// Expression
+document.write(5 + 3);
+
+// Escape characters
+var quote = "She said, \"JavaScript is fun!\"";
+window.alert(quote);
+
+// Event example (optional next step)
+function showMessage() {
+    alert("Button was clicked!");
+}
