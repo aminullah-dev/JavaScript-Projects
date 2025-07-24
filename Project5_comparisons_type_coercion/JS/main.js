97a35372c751ead0840d9b617b8a6dce81d1cd3f
aminullah-dev <Aminhashemi979@gmail.com>
2025-07-23 20:04:46 -0400

    Final Project6: Ternary, Constructor, Nested Function

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,5 @@
+document.getElementById("type").innerHTML = typeof 123;
+document.getElementById("coercion").innerHTML = "10" + 5;
+document.getElementById("equal").innerHTML = 10 == "10";
+document.getElementById("strictEqual").innerHTML = 10 === "10";
+document.getElementById("logic").innerHTML = (10 > 5 && 3 < 1) || !(5 < 2);
